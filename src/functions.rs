b2e58dd95ea3c826fdec0785590e9ba5cdf69ffe
Magic Len <len@magiclen.org>
2021-03-11 06:11:48 +0800

    run clippy

--- src/functions.rs
+++ src/functions.rs
@@ -8,16 +8,10 @@ pub(crate) fn str_bytes_fmt(v: &[u8], f: &mut Formatter) -> Result<(), fmt::Erro
 
 #[inline]
 pub(crate) fn is_whitespace(e: u8) -> bool {
-    match e {
-        0x09..=0x0D | 0x1C..=0x20 => true,
-        _ => false,
-    }
+    matches!(e, 0x09..=0x0D | 0x1C..=0x20)
 }
 
 #[inline]
 pub(crate) fn is_ascii_control(e: u8) -> bool {
-    match e {
-        0..=8 | 11..=31 | 127 => true,
-        _ => false,
-    }
+    matches!(e, 0..=8 | 11..=31 | 127)
 }
